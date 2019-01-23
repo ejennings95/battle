417e733d1cb768d5ccd717f12af1fc8e0f7e1464
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-23 14:59:07 +0000

    made a get route to play and used a redirect from the post names route

--- lib/app.rb
+++ lib/app.rb
@@ -1,16 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  set :session_secret, 'secret af'
+  enable :sessions
 
-get '/' do
-  erb(:index)
-end
+  get '/' do
+    erb(:index)
+  end
 
-post '/play' do
-  @player_1 = params[:player_1]
-  @player_2 = params[:player_2]
-  erb(:play)
-end
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:play)
+  end
 
-run! if app_file == $0
+  post '/names' do
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  run! if app_file == $0
 end
