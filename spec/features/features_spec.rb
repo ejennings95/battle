107903b134c14c1f09ff49592361437b2dafdd00
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-23 14:36:32 +0000

    created an index and play erb which allows player names to be added via a form

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,6 +1,16 @@
-feature 'Infrastructure test' do
-  scenario 'the hompage says Testing infrastructure working!' do
+# feature 'Infrastructure test' do
+#   scenario 'the hompage says Testing infrastructure working!' do
+#     visit '/'
+#     expect(page).to have_content('Testing infrastructure working!')
+#   end
+# end
+
+feature 'Enter pair names' do
+  scenario 'fill in two names in a form and see them on the screen' do
     visit '/'
-    expect(page).to have_content('Testing infrastructure working!')
+    fill_in 'player_1', with: 'name1'
+    fill_in 'player_2', with: 'name2'
+    click_button 'Submit'
+    expect(page).to have_content('name1 Vs. name2')
   end
 end
