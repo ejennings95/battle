d750808ae319543ea669c23b0dbd58f272741e8d
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-23 16:53:43 +0000

    Created the attack function and reset health function

--- lib/app.rb
+++ lib/app.rb
@@ -9,14 +9,27 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
+    session[:count] ||= 0
+    @health = (100 - session[:count])
     @player_1 = session[:player_1]
     @player_2 = session[:player_2]
     erb(:play)
   end
 
   post '/names' do
+    session.clear
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  post '/attack' do
+    session[:count] += 10
+    redirect '/play'
+  end
+
+  post '/reset' do
+    session[:count] = 0
     redirect '/play'
   end
 
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,14 +1,29 @@
 feature 'See the names of both players' do
   scenario 'to be able to see the names which were typed into the form' do
     sign_in_and_play
-    expect(page).to have_content 'name1 Vs. name2' 
+    expect(page).to have_content 'name1 Vs. name2'
   end
 end
 
   feature 'See opponents health' do
     scenario 'to be able to see how much health the opponent has left' do
       sign_in_and_play
-      expect(page).to have_content 'name1 health: 100HP'
+      expect(page).to have_content 'name2 health: 100HP'
+    end
+  end
+
+  feature 'confirmation of attack' do
+    scenario 'to be able to see a message that I have attacked my opponent' do
+      sign_in_and_play
+      click_button 'Attack'
+      expect(page).to have_content 'name2 health: 90HP'
+    end
+  end
+
+  feature 'confirmation of attack' do
+    scenario 'to be able to see a message that I have attacked my opponent' do
+      sign_in_and_play
+      click_button 'Reset'
       expect(page).to have_content 'name2 health: 100HP'
     end
   end
