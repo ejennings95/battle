f8ff67feabde7f36f826d435c0423e8aa2b1fafb
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-23 15:27:35 +0000

    added in a health section of play. tested to pqss

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,16 +1,20 @@
-# feature 'Infrastructure test' do
-#   scenario 'the hompage says Testing infrastructure working!' do
-#     visit '/'
-#     expect(page).to have_content('Testing infrastructure working!')
-#   end
-# end
-
-feature 'Enter pair names' do
-  scenario 'fill in two names in a form and see them on the screen' do
+feature 'See the names of both players' do
+  scenario 'to be able to see the names which were typed into the form' do
     visit '/'
     fill_in 'player_1', with: 'name1'
     fill_in 'player_2', with: 'name2'
     click_button 'Submit'
-    expect(page).to have_content('name1 Vs. name2')
+    expect(page.has_content?('name1 Vs. name2')).to eq true
   end
 end
+
+  feature 'See opponents health' do
+    scenario 'to be able to see how much health the opponent has left' do
+      visit '/'
+      fill_in 'player_1', with: 'name1'
+      fill_in 'player_2', with: 'name2'
+      click_button 'Submit'
+      expect(page.has_content?('name1 health: 100HP')).to eq true
+      expect(page.has_content?('name2 health: 100HP')).to eq true
+    end
+  end
