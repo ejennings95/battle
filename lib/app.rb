37cccdf23f982199f46954834311acb6fad9099f
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-25 12:10:38 +0000

    player class created and used instead of sessions

--- lib/app.rb
+++ lib/app.rb
@@ -9,34 +9,14 @@ class Battle < Sinatra::Base
 
   get '/play' do
     @player_1_name = $player_1.name
-    @player_2_name = $player_2
-    p 'play'
-    p $player_1
-    p $player_2
-    p $player_1_name
-    p @player_1_name
-    p $player_2_health
-    p @player_2_name
-    p @player_2_health
+    @player_2_name = $player_2.name
+    @player_2_health = $player_2.hp
     erb(:play)
   end
 
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
-    p params
-
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_2_health = $player_2.hp
-    p 'post'
-    p $player_1
-    p $player_2
-    p $player_1.name
-    p @player_1_name
-    p $player_2.name
-    p @player_2_name
-    p @player_2_health
     redirect '/play'
   end
 
